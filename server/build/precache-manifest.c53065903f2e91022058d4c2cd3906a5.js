87de3361bb13ae71eeb94e7559ed4562a452e156
jainpriyanshi <jainpriyanshi2407@gmail.com>
2020-08-25 21:21:16 +0530

    deployment fixes

---
+++ server/build/precache-manifest.c53065903f2e91022058d4c2cd3906a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e65c1885cb327ad46c79986571e5116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b0b11c39f011cc2c93",
+    "url": "/static/css/main.4a7fc795.chunk.css"
+  },
+  {
+    "revision": "7a001bc108c1eb05586b",
+    "url": "/static/js/2.0a891628.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/static/js/2.0a891628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b0b11c39f011cc2c93",
+    "url": "/static/js/main.2bcf92ad.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b395fddebdf12e7ed023e9cce9cdbad1",
+    "url": "/static/media/Prescription1.b395fdde.jpeg"
+  },
+  {
+    "revision": "f46212097ba1f8bc97a378c1ddd3f3dd",
+    "url": "/static/media/bg.f4621209.png"
+  }
+]);
